2ef0de5ffdb69d51321a9ee65169b45ebe5c88ff
Constantin Titarenko <constantin.titarenko@gmail.com>
2019-03-08 23:57:15 +0200

    add shebang, add module resolution

--- src/run.ts
+++ src/run.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import glob from 'glob'
 
 import Config from './Config'
@@ -5,7 +7,7 @@ import Config from './Config'
 function run () {
   const config = Config.getCurrent()
   const files = glob.sync(config.shotFiles)
-  files.forEach(f => require(f))
+  files.forEach(f => require(process.cwd() + '/' + f))
 }
 
 run()
