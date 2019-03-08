ae7c10a45740d49d5fbf91cd3c4a893e92d826ee
Constantin Titarenko <constantin.titarenko@gmail.com>
2019-03-08 23:54:27 +0200

    session: fix chainability, fix page.evaluate serialization issue

--- src/ShotApi.ts
+++ src/ShotApi.ts
@@ -7,32 +7,38 @@ import Image from './Image'
 class ShotApi {
   private config: Config
   private session: Session
+  private queue: Promise<any>
 
   constructor (config: Config, session: Session) {
     this.config = config
     this.session = session
+    this.queue = Promise.resolve()
   }
 
-  async goTo (relativeUrl: string): Promise<ShotApi> {
-    const page = await this.session.getPage()
-    await page.goto(this.config.baseUrl + relativeUrl, { waitUntil: 'networkidle0' })
+  goTo (relativeUrl: string): ShotApi {
+    this.queue = this.queue.then(async () => {
+      const page = await this.session.getPage()
+      await page.goto(this.config.baseUrl + relativeUrl, { waitUntil: 'networkidle0' })
+    })
     return this
   }
 
-  async evaluate (fn: puppeteer.EvaluateFn, ...args: puppeteer.SerializableOrJSHandle[]): Promise<ShotApi> {
-    const page = await this.session.getPage()
-    await page.evaluate(fn, ...args)
+  evaluate (fn: puppeteer.EvaluateFn, ...args: puppeteer.SerializableOrJSHandle[]): ShotApi {
+    this.queue = this.queue.then(async () => {
+      const page = await this.session.getPage()
+      await page.evaluate(fn, ...args)
+    })
     return this
   }
 
   async takeScreenshot (selector: string): Promise<Image> {
+    await this.queue
     const page = await this.session.getPage()
-    const rect: puppeteer.BoundingBox = await page.evaluate(selector => {
+    const serializedRect = await page.evaluate(selector => {
       const element = document.querySelector(selector)
-      return element
-        ? element.getBoundingClientRect()
-        : null
+      return element && JSON.stringify(element.getBoundingClientRect())
     }, selector)
+    const rect = serializedRect && JSON.parse(serializedRect)
     if (!rect) {
       throw new Error(`cannot get boundaries of element by selector "${selector}"`)
     }
